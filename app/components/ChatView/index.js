0b071bbc4d7b0fd2d65c9707fe569ed9f988b71f
Vyacheslav Alayev <valayev1@gmail.com>
2017-08-19 19:01:38 -0400

    Refactor chats and languages redux modules

--- app/components/ChatView/index.js
+++ app/components/ChatView/index.js
@@ -21,7 +21,7 @@ import { AutoExpandingTextField } from '../../components/Form';
 import InvertibleScrollView from 'react-native-invertible-scroll-view';
 import ChatRow from './components/ChatRow';
 import Speech from 'react-native-speech';
-import { clearNewMessage, updateNewMessage, clearChatBadges } from '../../redux/chat';
+import { clearNewMessage, updateNewMessage, clearChatBadges } from '../../redux/chat.js';
 
 const VOICE_LANG_CODES = ['ar-SA', 'cs-CZ', 'da-DK', 'de-DE', 'el-GR', 'en-AU', 'en-GB', 'en-IE', 'en-US', 'en-ZA', 'es-ES', 'es-MX', 'fi-FI', 'fr-CA', 'fr-FR', 'he-IL', 'hi-IN', 'hu-HU', 'id-ID', 'it-IT', 'ja-JP', 'ko-KR', 'nl-BE', 'nl-NL', 'no-NO', 'pl-PL', 'pt-BR', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK', 'sv-SE', 'th-TH', 'tr-TR', 'zh-CN', 'zh-HK', 'zh-TW']
     .reduce((acc, code) => {
--- app/redux/chat.js
+++ app/redux/chat.js
@@ -4,15 +4,10 @@ import { BASE_URL } from '../constants/AppConstants';
 
 export const UPDATE_NEW_MESSAGE = 'UPDATE_NEW_MESSAGE';
 export const CLEAR_NEW_MESSAGE = 'CLEAR_NEW_MESSAGE';
-export const REQUEST_GET_CHATS = 'REQUEST_GET_CHATS';
-export const RECEIVE_GET_CHATS = 'RECEIVE_GET_CHATS';
-export const FAILURE_GET_CHATS = 'FAILURE_GET_CHATS';
-export const REQUEST_CREATE_CHAT = 'REQUEST_CREATE_CHATS';
-export const RECEIVE_CREATE_CHAT = 'RECEIVE_CREATE_CHATS';
-export const REQUEST_UPDATE_SETTINGS = 'REQUEST_UPDATE_SETTINGS';
-export const RECEIVE_UPDATE_SETTINGS = 'RECEIVE_UPDATE_SETTINGS';
-export const REQUEST_CLEAR_CHAT_BADGES = 'REQUEST_CLEAR_CHAT_BADGES';
-export const RECEIVE_CLEAR_CHAT_BADGES = 'RECEIVE_CLEAR_CHAT_BADGES';
+export const GET_CHATS = 'GET_CHATS';
+export const CREATE_CHAT = 'CREATE_CHATS';
+export const UPDATE_SETTINGS = 'UPDATE_SETTINGS';
+export const CLEAR_CHAT_BADGES = 'CLEAR_CHAT_BADGES';
 export const SET_GLOBAL_BADGE_NUMBER = 'SET_GLOBAL_BADGE_NUMBER';
 
 export function updateNewMessage(groupId, message) {
@@ -23,23 +18,9 @@ export function clearNewMessage(groupId) {
     return { type: CLEAR_NEW_MESSAGE, payload: { groupId } };
 }
 
-function requestGetChats() {
-    return { type: REQUEST_GET_CHATS };
-}
-
-function receiveGetChats(payload) {
-    return { type: RECEIVE_GET_CHATS, payload };
-}
-
-function failureGetChats() {
-    return { type: FAILURE_GET_CHATS };
-}
-
-export function getChats() {
-    return (dispatch) => {
-        dispatch(requestGetChats());
-
-        return AsyncStorage.getItem('AUTH_TOKEN').then((value) => {
+export const getChats = () => ({
+    type: GET_CHATS,
+    payload: AsyncStorage.getItem('AUTH_TOKEN').then((value) => {
             return fetch(`${BASE_URL}chats`, {
                 headers: {
                     Authorization: `JWT ${value}`
@@ -47,27 +28,14 @@ export function getChats() {
             });
         })
         .then((response) => response.json())
-        .then((res) => {
-            dispatch(receiveGetChats(res.data));
-            return res.data;
-        })
-        .catch(() => dispatch(failureGetChats()));
-    }
-}
+        .then((res) => res.data)
+        .catch((err) => console.log(err)) // eslint-disable-line
+});
 
-function requestCreateChat() {
-    return { type: REQUEST_CREATE_CHAT };
-}
-
-function receiveCreateChat(payload) {
-    return { type: RECEIVE_CREATE_CHAT, payload };
-}
-
-export function createChat(payload) {
-    return (dispatch) => {
-        dispatch(requestCreateChat());
-
-        return AsyncStorage.getItem('AUTH_TOKEN').then((value) => {
+export const createChat = (payload) => ({
+    type: CREATE_CHAT,
+    payload: AsyncStorage.getItem('AUTH_TOKEN')
+        .then((value) => {
             return fetch(`${BASE_URL}chats`, {
                 method: 'post',
                 headers: {
@@ -79,77 +47,54 @@ export function createChat(payload) {
             });
         })
         .then((response) => response.json())
-        .then((res) => {
-            dispatch(receiveCreateChat(res.data));
-            return res.data;
-        });
-        // .catch((err) => console.log(err)); // eslint-disable-line
-    }
-}
+        .then((res) => res.data)
+});
 
-function requestUpdateSettings() {
-    return { type: REQUEST_UPDATE_SETTINGS };
-}
-
-function receiveUpdateSettings(payload) {
-    return { type: RECEIVE_UPDATE_SETTINGS, payload };
-}
-
-export function updateSettings(data) {
-    return (dispatch) => {
-        dispatch(requestUpdateSettings());
-
-        return AsyncStorage.getItem('AUTH_TOKEN')
-            .then((value) => fetch(`${BASE_URL}chats/settings`, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `JWT ${value}`
-                },
-                body: JSON.stringify(data)
-            }))
+export const updateSettings = (data) => ({
+    type: UPDATE_SETTINGS,
+    payload: AsyncStorage.getItem('AUTH_TOKEN')
+        .then((value) => fetch(`${BASE_URL}chats/settings`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${value}`
+            },
+            body: JSON.stringify(data)
+        }))
         .then((response) => response.json())
-        .then((res) => {
-            dispatch(receiveUpdateSettings(res.data));
-        });
-    };
-}
+        .then((res) => res.data)
+});
 
-function requestClearChatBadges() {
-    return { type: REQUEST_CLEAR_CHAT_BADGES };
-}
-
-function receiveClearChatBadges(payload) {
-    return { type: RECEIVE_CLEAR_CHAT_BADGES, payload };
-}
-
-export function clearChatBadges(groupId) {
+export const clearChatBadges = (groupId) => {
     return (dispatch) => {
-        dispatch(requestClearChatBadges());
+        return dispatch({
+            type: CLEAR_CHAT_BADGES,
+            payload: AsyncStorage.getItem('AUTH_TOKEN')
+                .then((value) => {
+                    return fetch(`${BASE_URL}chats/clear-badges`, {
+                        method: 'post',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'Authorization': `JWT ${value}`
+                        },
+                        body: JSON.stringify({ group_id: groupId })
+                    });
+                })
+                .then((response) => response.json())
+                .then((res) => {
+                    const totalBadges = res.data.reduce((acc, group) => acc + group.badges, 0);
 
-        return AsyncStorage.getItem('AUTH_TOKEN').then((value) => {
-            return fetch(`${BASE_URL}chats/clear-badges`, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `JWT ${value}`
-                },
-                body: JSON.stringify({ group_id: groupId })
-            });
-        })
-        .then((response) => response.json())
-        .then((res) => {
-            const totalBadges = res.data.reduce((acc, group) => acc + group.badges, 0);
+                    dispatch(setGlobalBadgeNumber(totalBadges));
+                    PushNotificationIOS.setApplicationIconBadgeNumber(totalBadges);
 
-            dispatch(receiveClearChatBadges(res.data));
-            dispatch(setGlobalBadgeNumber(totalBadges));
-            PushNotificationIOS.setApplicationIconBadgeNumber(totalBadges);
-        })
-        .catch((err) => console.log(err)); // eslint-disable-line
+                    return res.data;
+                })
+                .catch((err) => console.log(err)) // eslint-disable-line
+        });
     }
-}
+};
 
 export function setGlobalBadgeNumber(number) {
     return { type: SET_GLOBAL_BADGE_NUMBER, payload: number };
@@ -177,14 +122,14 @@ export default function reducer(state = initialState, action) {
         case CLEAR_NEW_MESSAGE:
             state.newMessageText[action.payload.groupId] = '';
             return { ...state };
-        case RECEIVE_GET_CHATS:
-        case RECEIVE_CLEAR_CHAT_BADGES:
+        case `${GET_CHATS}_FULFILLED`:
+        case `${CLEAR_CHAT_BADGES}_FULFILLED`:
             return { ...state, allChats: action.payload.sort(compareChats) };
         case SET_GLOBAL_BADGE_NUMBER:
             return { ...state, chatBadgeNumber: action.payload };
-        case RECEIVE_CREATE_CHAT:
+        case `${CREATE_CHAT}_FULFILLED`:
             return { ...state, allChats: [...action.payload, ...state.allChats] };
-        case RECEIVE_UPDATE_SETTINGS:
+        case `${UPDATE_SETTINGS}_FULFILLED`:
             return {
                 ...state,
                 allChats: state.allChats
