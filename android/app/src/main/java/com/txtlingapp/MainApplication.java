3332e3ae7d19618cee5560ea88816f436f2664b6
Vyacheslav Alayev <valayev1@gmail.com>
2017-12-05 23:29:58 -0500

    Relink

--- android/app/src/main/java/com/txtlingapp/MainApplication.java
+++ android/app/src/main/java/com/txtlingapp/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.lwansbrough.ReactCamera.ReactCamera;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new GoogleAnalyticsBridgePackage(),
+            new ReactNativeContacts(),
             new ReactCamera()
       );
     }
